c316d734e4941d5fe9218a73abe7094eecb06689
Avinash Wadhawan <avinashxw@gmail.com>
2021-07-05 23:53:29 +0530

    todoitems-table-added

--- database/migrations/2021_07_05_182210_create_todoitems_table.php
+++ database/migrations/2021_07_05_182210_create_todoitems_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTodosTable extends Migration
+class CreateTodoitemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTodosTable extends Migration
      */
     public function up()
     {
-        Schema::create('todos', function (Blueprint $table) {
+        Schema::create('todoitems', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateTodosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todos');
+        Schema::dropIfExists('todoitems');
     }
 }
