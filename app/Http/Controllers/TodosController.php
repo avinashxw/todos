c8b66bc23075a25e9293163b66bfcdcbe3b1b622
Avinash Wadhawan <avinashxw@gmail.com>
2021-07-05 23:44:51 +0530

    mcr-direct

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todos;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todos $todos)
+    {
+        //
+    }
+}
